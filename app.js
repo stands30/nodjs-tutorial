c731c6ed9e921f36d341fe88841df4d01e27d76b
stands30 <lancylotdsouza@gmail.com>
2022-01-15 12:37:32 +0530

    html files added and links served statically

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const rootDir = require('./util/path');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+//  parse the body request
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(rootDir, 'public')));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next) =>{
-    // console.log(' this always runs');
-    next();
+app.use((req, res, next) =>{
+    res.sendFile(path.join(rootDir, 'views','404.html'));
 });
 
-app.get('/add-product',(req, res, next) =>{
-    // console.log(' in add-product middleware');
-    // res.send('<h1>Add product</h1>');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit" >Add Product </button></form>');
-});
-
-app.post('/product', (req, res, next) =>{
-    // console.log(' in product middleware');
-    console.log('req body : ', req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) =>{
-    // console.log(' in another middleware');
-    res.send('<h1>Hello from Express</h1>');
-});
 
 // const server = http.createServer(app);
 
