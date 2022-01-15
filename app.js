6afccc717036ea7e8b33501367dd06af09a8eb26
stands30 <lancylotdsouza@gmail.com>
2022-01-15 10:34:18 +0530

    understanding npm scriipts

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http = require('http');
-const routes = require('./routes')
+
+const routes = require('./routes');
+
+console.log(routes.someText);
+
 const server = http.createServer(routes.handler);
 
 server.listen(3000);
