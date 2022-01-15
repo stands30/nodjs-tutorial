cf3e7d626118cb46b1f8d6ae6ab83f663aed3741
stands30 <lancylotdsouza@gmail.com>
2022-01-15 11:11:34 +0530

    body parser, route handling in express js

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use(bodyParser.urlencoded());
 
-const server = http.createServer(routes.handler);
+app.use('/',(req, res, next) =>{
+    // console.log(' this always runs');
+    next();
+});
 
-server.listen(3000);
+app.get('/add-product',(req, res, next) =>{
+    // console.log(' in add-product middleware');
+    // res.send('<h1>Add product</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit" >Add Product </button></form>');
+});
+
+app.post('/product', (req, res, next) =>{
+    // console.log(' in product middleware');
+    console.log('req body : ', req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) =>{
+    // console.log(' in another middleware');
+    res.send('<h1>Hello from Express</h1>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
+
+
