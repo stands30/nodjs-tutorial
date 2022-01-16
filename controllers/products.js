f2201620ea2e427597b3e9de4fe0e4703587f5c5
stands30 <lancylotdsouza@gmail.com>
2022-01-16 10:06:02 +0530

    model created

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,6 @@
-const products = [];
+
+const Product = require('../models/product');
+
 exports.getAddProduct = (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views','add-product.html'));
     // Render view
@@ -15,12 +17,14 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) =>{
     console.log('req body : ', req.body);
     if(req.body.title){
-        products.push({title:req.body.title});
+        const product = new Product(req.body.title);
+        product.save();
     }
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) =>{
+    const products =  Product.fetchAll();
     res.render('shop', {
         docTitle: 'Shop',
         prods: products,
